1eafdce81cd625eb9965544d7d0b0ca515443238
AbakumovaOlga <31732995+AbakumovaOlga@users.noreply.github.com>
2022-06-01 13:19:00 +0400

    Model model

--- src/main/java/ru/abakumova/springpetmvc/controllers/FirstController.java
+++ src/main/java/ru/abakumova/springpetmvc/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.abakumova.springpetmvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,15 +13,34 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(@RequestParam(value = "name") String name, @RequestParam(value = "surname", required = false) String surname) {
-
-        System.out.println(name + ", " + surname);
-        System.out.println(surname == null);
+    public String helloPage(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "surname", required = false) String surname, Model model) {
+        model.addAttribute("message", "hello, " + name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a") int a, @RequestParam(value = "b") int b, @RequestParam(value = "action") String action, Model model) {
+        switch (action){
+            case "multiplication":{
+                model.addAttribute("result", a*b);
+                break;
+            }
+            case "addition":{
+                model.addAttribute("result", a+b);
+                break;
+            }
+            case "division":{
+                model.addAttribute("result", a/b);
+                break;
+            }
+        }
+
+        return "first/goodbye";
+
     }
 }
